898e20ec523882bbd2d5b2df3836149f94650613
bbudionoacnpersonal <b.budiono@accenture.com>
2025-06-05 16:48:40 +0700

    Updated ConversationAnalysis.tsx

--- src/components/create-agent/ConversationAnalysis.tsx
+++ src/components/create-agent/ConversationAnalysis.tsx
@@ -521,8 +521,7 @@ const ConversationAnalysis: React.FC<ConversationAnalysisProps> = ({
                                       {tool.params_as_json && (
                                         <div className="bg-dark-400/50 p-1 rounded mt-1">
                                           <pre className="text-xs text-gray-300 overflow-x-auto">
-                                            tool.name 
-                                            {JSON.stringify(tool.params_as_json, null, 2)}
+JSON.stringify(tool.name, null, 2)                                            {JSON.stringify(tool.params_as_json, null, 2)}
                                           </pre>
                                         </div>
                                       )}
