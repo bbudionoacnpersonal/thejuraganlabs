4c274eb9177b5a7bc2457c980247bef40f60d083
bbudionoacnpersonal <b.budiono@accenture.com>
2025-06-05 19:05:13 +0700

    Updated ConversationAnalysis.tsx

--- src/components/create-agent/ConversationAnalysis.tsx
+++ src/components/create-agent/ConversationAnalysis.tsx
@@ -544,7 +544,7 @@ const ConversationAnalysis: React.FC<ConversationAnalysisProps> = ({
                                             {JSON.stringify(tool.params_as_json, null, 2)}
                                             if tool.tool_name === "task_generator"
                                             (
-                                                  setTaskData(params.task);
+                                                  setTaskDatatool.params_as_jso);
                                             )
                                           </pre>
                                         </div>
