29c5d04d1673a3b7cbe40dc64af9d5dbfbcb8a83
bbudionoacnpersonal <b.budiono@accenture.com>
2025-06-05 23:18:52 +0700

    Updated TranscriptHandler.tsx

--- src/components/create-agent/TranscriptHandler.tsx
+++ src/components/create-agent/TranscriptHandler.tsx
@@ -142,7 +142,7 @@ const TranscriptHandler: React.FC<TranscriptHandlerProps> = ({
                   <Button
                     onClick={handleSubmitTranscript}
                     isLoading={isLoading}
-                    fullWidth="'"true"
+                    fullWidth=""true"
                     className="items-center justify-center"
                   >
                     Generate AI Agent Code
