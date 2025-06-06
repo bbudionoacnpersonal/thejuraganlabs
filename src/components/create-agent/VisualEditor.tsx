cd5d65777f400e168d33792360381847d0c46143
bbudionoacnpersonal <b.budiono@accenture.com>
2025-06-06 19:37:20 +0700

    Updated VisualEditor.tsx

--- src/components/create-agent/VisualEditor.tsx
+++ src/components/create-agent/VisualEditor.tsx
@@ -116,7 +116,7 @@ const VisualEditor: React.FC<VisualEditorProps> = ({ teamStructure }) => {
         fitView
         minZoom={0.2}
         maxZoom={1.2}
-        defaultViewport={{ x: 0, y: 0, zoom: 0.5 }}
+        defaultViewport={{ x: 0, y: 0, zoom: 0.7 }}
         fitViewOptions={{ 
           padding: 0.2,
           includeHiddenNodes: true,
