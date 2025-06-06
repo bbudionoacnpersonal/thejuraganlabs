8ddf77790e8d4919e27c86434c38cab896de71e9
bbudionoacnpersonal <b.budiono@accenture.com>
2025-06-06 12:12:32 +0700

    Updated VoiceSDKOverlay.tsx

--- src/components/create-agent/VoiceSDKOverlay.tsx
+++ src/components/create-agent/VoiceSDKOverlay.tsx
@@ -130,7 +130,7 @@ const VoiceSDKOverlay: React.FC<VoiceSDKOverlayProps> = ({
         context: {
           industry: userIndustry,
           focus_areas: userFocusAreas,
-          industry_config: industries.find(i => i.value === userIndustry)?.keyPrompts || {},
+          industry_config: userIndustry)?.keyPrompts || {},
           focus_area_configs: focusAreas.filter(fa => userFocusAreas.includes(fa.value))
         },
         prompt: {
