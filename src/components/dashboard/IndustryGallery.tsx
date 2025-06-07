da02f3f9dd0d0fe082c8a19b4baaa234de2524d3
bbudionoacnpersonal <b.budiono@accenture.com>
2025-06-07 23:16:06 +0700

    Updated IndustryGallery.tsx

--- src/components/dashboard/IndustryGallery.tsx
+++ src/components/dashboard/IndustryGallery.tsx
@@ -36,7 +36,8 @@ const IndustryGallery: React.FC<IndustryGalleryProps> = ({ userIndustry, userFoc
   //const focusAreaDetails = focusAreas.filter(f => userFocusAreas.includes(f.value));
 
   // Get use cases based on user's industry and focus areas
-  const recommendedUseCases = getUseCasesByIndustryAndFunction(userIndustry, userFocusAreas);
+  //const recommendedUseCases = getUseCasesByIndustryAndFunction(userIndustry, userFocusAreas);
+  const recommendedUseCases = getUseCasesByIndustryAndFunction(userIndustry);
   const popularUseCases = getPopularUseCases();
 
   const getDifficultyColor = (difficulty: string) => {
