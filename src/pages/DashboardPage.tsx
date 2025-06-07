d7a09d9489fe493ae4c992b3beebd497af25ffa3
bbudionoacnpersonal <b.budiono@accenture.com>
2025-06-07 22:41:51 +0700

    Updated DashboardPage.tsx

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -207,14 +207,12 @@ const DashboardPage: React.FC = () => {
           )}
 
           {/* Industry Gallery Card - Only show if user has selected industry and focus areas */}
-          <div className="grid grid-cols-1 lg:grid-cols-1 gap-8 mt-8">
           {hasIndustryAndFocus && (
             <IndustryGallery 
               userIndustry={userIndustry}
               userFocusAreas={userFocusAreas}
             />
           )}
-            </div>
         </div>
       </main>
       <Footer />
