afc64fd60e6ead3fe0667015a6a91f22d10ec63d
bbudionoacnpersonal <b.budiono@accenture.com>
2025-06-08 09:39:19 +0700

    Updated industry_functions.ts

--- src/mockdata/industry_functions.ts
+++ src/mockdata/industry_functions.ts
@@ -64,7 +64,7 @@ export const industries = [
     }
   },
   { 
-    value: 'manufacturing_cpg', // MERGED
+    value: 'cpg_manufacturing', // MERGED
     label: 'Consumer Goods & Manufacturing', // MERGED
     keyPrompts: {
       agentConsiderations: [
