6a2faf908b14ef44885131f0aceef0eb9cfa7734
bbudionoacnpersonal <b.budiono@accenture.com>
2025-06-08 07:42:41 +0700

    Updated IndustryGallery.tsx

--- src/components/dashboard/IndustryGallery.tsx
+++ src/components/dashboard/IndustryGallery.tsx
@@ -145,7 +145,7 @@ const IndustryGallery: React.FC<IndustryGalleryProps> = ({
               <Button
                 variant="ghost"
                 size="sm"
-                className="border boder-dark-border"
+                className="boder-dark-border"
                 onClick={() => {
                   setCurrentFilterIndustry('');
                   setCurrentFilterFunctionAreas([]);
